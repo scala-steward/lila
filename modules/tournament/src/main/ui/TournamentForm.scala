scala-steward/lila

commit 36a74eaf58bb204931d282b8d96d0af4c5d76309

Arena new tournament creation: standardise checkbox/toggle UX

modules/tournament/src/main/ui/TournamentForm.scala
@@ -162,18 +162,46 @@ final class TournamentForm(val helpers: Helpers, showUi: TournamentShow)(
       )
     )
 
+  private def nativeCheckboxField(
+      field: Field,
+      labelContent: Frag,
+      help: Option[Frag],
+      half: Boolean = false,
+      value: String = "true"
+  ) =
+    div(
+      cls := List(
+        "form-check form-group" -> true,
+        "form-half" -> half
+      )
+    )(
+      div(
+        span(cls := "form-check-input")(
+          form3.nativeCheckbox(
+            form3.id(field),
+            field.name,
+            checked = field.value.has("true"),
+            value = value
+          )
+        ),
+        label(`for` := form3.id(field))(labelContent)
+      ),
+      help.map(small(cls := "form-help")(_))
+    )
+
   def featuresFields(form: Form[?])(using ctx: Context)(using FormPrefix) =
     form3.fieldset("Features", toggle = false.some)(
       form3.split(
-        form3.checkbox(
-          form.prefix("berserkable"),
+        nativeCheckboxField(
+          form("berserkable"),
           trans.arena.allowBerserk(),
           help = trans.arena.allowBerserkHelp().some,
-          half = true
+          half = true,
+          value = "1"
         ),
         form3.hiddenFalse(form.prefix("berserkable")),
-        form3.checkbox(
-          form.prefix("streakable"),
+        nativeCheckboxField(
+          form("streakable"),
           trans.arena.arenaStreaks(),
           help = trans.arena.arenaStreaksHelp().some,
           half = true
@@ -181,14 +209,14 @@ final class TournamentForm(val helpers: Helpers, showUi: TournamentShow)(
         form3.hiddenFalse(form.prefix("streakable"))
       ),
       form3.split(
-        form3.checkbox(
-          form.prefix("rated"),
+        nativeCheckboxField(
+          form("rated"),
           trans.site.rated(),
           help = trans.site.ratedFormHelp().some
         ),
         form3.hiddenFalse(form.prefix("rated")),
-        form3.checkbox(
-          form.prefix("hasChat"),
+        nativeCheckboxField(
+          form("hasChat"),
           trans.site.chatRoom(),
           help = trans.arena.allowChatHelp().some,
           half = true

modules/gathering/src/main/ui/GatheringUi.scala
@@ -114,14 +114,24 @@ final class GatheringFormUi(helpers: Helpers):
     )
 
   def bots(field: Field, disabledAfterStart: Boolean) =
-    form3.checkbox(
-      field,
-      "Allow bot accounts",
-      help = frag(
-        "Let ",
-        a(href := "/@/lichess/blog/welcome-lichess-bots/WvDNticA")("bots"),
-        " join the tournament and play with their engines. This often repels human players."
-      ).some,
-      half = true,
-      disabled = disabledAfterStart
+    val disabledAttr = disabledAfterStart.option(disabled)
+    div(cls := "form-group")(
+      div(
+        span(cls := "form-check-input")(
+          form3.nativeCheckbox(
+            field.id,
+            field.name,
+            checked = field.value.has("true"),
+            value = "1"
+          )(disabledAttr.toSeq*)
+        ),
+        label(`for` := field.id)("Allow bot accounts")
+      ),
+      div(cls := "form-help")(
+        frag(
+          "Let ",
+          a(href := "/@/lichess/blog/welcome-lichess-bots/WvDNticA")("bots"),
+          " join the tournament and play with their engines. This often repels human players."
+        ).some
+      )
     )

commit 7eaefedbbacd5d8e884933af976d3c6a7ef97dad

fix tournament form features
broken by 36a74eaf58bb204931d282b8d96d0af4c5d76309

modules/tournament/src/main/ui/TournamentForm.scala
@@ -166,14 +166,14 @@ final class TournamentForm(val helpers: Helpers, showUi: TournamentShow)(
     form3.fieldset("Features", toggle = false.some)(
       form3.split(
         form3.checkboxGroup(
-          form("berserkable"),
+          form.prefix("berserkable"),
           trans.arena.allowBerserk(),
           help = trans.arena.allowBerserkHelp().some,
           half = true
         ),
         form3.hiddenFalse(form.prefix("berserkable")),
         form3.checkboxGroup(
-          form("streakable"),
+          form.prefix("streakable"),
           trans.arena.arenaStreaks(),
           help = trans.arena.arenaStreaksHelp().some,
           half = true
@@ -182,14 +182,14 @@ final class TournamentForm(val helpers: Helpers, showUi: TournamentShow)(
       ),
       form3.split(
         form3.checkboxGroup(
-          form("rated"),
+          form.prefix("rated"),
           trans.site.rated(),
           help = trans.site.ratedFormHelp().some,
           half = true
         ),
         form3.hiddenFalse(form.prefix("rated")),
         form3.checkboxGroup(
-          form("hasChat"),
+          form.prefix("hasChat"),
           trans.site.chatRoom(),
           help = trans.arena.allowChatHelp().some,
           half = true