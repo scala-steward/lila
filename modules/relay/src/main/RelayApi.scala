e493c9012029d8f0f261fc8e1d1b8a1034962331
Sérgio Glórias <9739913+SergioGlorias@users.noreply.github.com>
2026-02-19 02:44:03 +0000

    fix: enhance grouping logic to consider tour ownership and official status

--- modules/relay/src/main/RelayApi.scala
+++ modules/relay/src/main/RelayApi.scala
@@ -254,9 +254,12 @@ final class RelayApi(
       (tour.id :: data.grouping.so(_.tourIds)).foreach(withTours.invalidate)
 
   private def updateGrouping(tour: RelayTour, data: RelayGroupData)(using me: Me): Funit =
-    (Granter(_.Relay) || !tour.official).so:
-      val canGroup = fuccess(Granter(_.StudyAdmin)) >>| tourRepo.isOwnerOfAll(me.userId, data.tourIds)
-      canGroup.flatMapz(groupRepo.update(tour.id, data))
+      for
+        isOwner <- fuccess(Granter(_.StudyAdmin)) >>| tourRepo.isOwnerOfAll(me.userId, data.tourIds)
+        tours <- tourRepo.byIds(data.tourIds)
+        hasOfficial = tours.exists(_.official)
+        _ <- (isOwner && (!hasOfficial || Granter(_.Relay))).so(groupRepo.update(tour.id, data))
+      yield ()
 
   def create(data: RelayRoundForm.Data, tour: RelayTour)(using me: Me): Fu[RelayRound.WithTourAndStudy] = for
     last <- roundRepo.lastByTour(tour)
