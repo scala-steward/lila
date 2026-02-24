ec45155c8ec908729f3f8296c505e10ce610c486
johndoknjas <jdoknjas@sfu.ca>
2026-02-23 23:58:31 -0800

    Add shortcut to search for chapters in a study, and give shift+s to it instead of edit study.

--- modules/web/src/main/ui/help.scala
+++ modules/web/src/main/ui/help.scala
@@ -129,7 +129,8 @@ object help:
                 trans.site.toggleObservationAnnotations()
               ),
               row(frag(kbd("ctrl"), kbd("z")), "Undo arrow changes"),
-              row(frag(kbd("shift"), kbd("S")), trans.study.editStudy()),
+              row(frag(kbd("shift"), kbd("S")), "Search study chapters"),
+              row(frag(kbd("shift"), kbd("H")), trans.study.editStudy()),
               row(frag(kbd("shift"), kbd("E")), trans.study.editChapter()),
               row(frag(kbd("shift"), kbd("N")), trans.study.addNewChapter())
             )
